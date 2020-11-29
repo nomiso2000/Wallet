79ee2cbde4ed7abbac3ce3c0ef8c445411440446
kovalchukaiew <kovalchukaiew@gmail/com>
2020-11-29 23:32:43 +0200

    login_component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { ButtonEnter } from './buttons/buttonEnter/ButtonEnter';
+import { ButtonRegistration } from './buttons/buttonRegistration/ButtonRegistration';
+import { Container } from './container/Container';
+import { EmailForm } from './EmailForm/EmailForm';
+import { PassForm } from './PassForm/PassForm';
+import { FormWrapper } from './FormWrapper/FormWrapper';
+import styles from './Login.module.css';
+import { ImgMain } from './ImageMain/ImageMain';
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <Container>
+                <div className={styles.grayBG}>
+                  <FormWrapper>
+                    <p className={styles.p}>Wallet</p>
+                    <EmailForm />
+                    <PassForm />
+                    <ButtonEnter />
+                    <ButtonRegistration />    
+                  </FormWrapper>
+                    <div className={styles.pinkCircle}></div> 
+                </div>    
+
+                    <ImgMain/>    
+                    <div className={styles.violetCircle}></div>
+                    <span className={styles.span}>Finance App</span>
+            </Container>
+            
+        )
+    }
+    
+}
---
+++ src/components/Login/buttons/buttonEnter/ButtonEnter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../buttonEnter/ButtonEnter.module.css';
+
+
+export function ButtonEnter() {
+    return (
+      
+      <button type="submit"  className={styles.Button}>
+        ВХОД
+      </button>
+      
+    );
+  }
