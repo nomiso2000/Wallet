cf96d2401c6ac2eccafd017d1dabf7c4dc13649e
Elina <elisonfog@gmail.com>
2020-11-29 16:20:54 +0200

    changes to .docs

--- src/components/Currency/CurrencyAPI.js
+++ src/components/Currency/CurrencyAPI.js
@@ -1,12 +1,6 @@
 // import axios from 'axios';
 
 const getCurrencyValue = () => {
-<<<<<<< HEAD
-  return axios
-    .get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
-    .then(response => response.data)
-    .catch(error => console.log(error));
-=======
   return fetch(
     'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
   )
@@ -16,7 +10,6 @@ const getCurrencyValue = () => {
     .then(data => {
       return data;
     });
->>>>>>> dev
 };
 
 export default { getCurrencyValue };
