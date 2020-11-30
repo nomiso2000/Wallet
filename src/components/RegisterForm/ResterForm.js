9ed7ef8707975bf4b0de4d697a5c16122e2eb78c
Ievgenii <bijibnui88@gmail.com>
2020-11-30 23:59:25 +0200

    add

--- src/components/RegisterForm/ResterForm.js
+++ src/components/RegisterForm/ResterForm.js
@@ -3,14 +3,17 @@ import selectsvg from '../../styles/css/icon/formsvgfile.svg';
 import masage from '../../styles/css/icon/masage.svg';
 import profil from '../../styles/css/icon/profil.svg';
 import regist from '../../styles/css/icon/regist.svg';
-import sirclesvg from '../../styles/css/icon/orangesircle.svg'
-import style from './resterForm.module.scss';
+import fioletsvg from '../../styles/css/icon/fiolet.svg';
+import sirclesvg from '../../styles/css/icon/orangesircle.svg';
+import Ceshsvg from '../../styles/css/icon/Cesh.svg';
+import style from './resterForm.module.css';
 
 const ResterForm = () => {
   return (
     <div className={style.blockCover}>
    <div className={style.firstblock}>
    <img  className= {style.imgrigister} src={selectsvg}/>
+   <img className={style.fiolet} src={fioletsvg}/>
 <h2 className={style.titleselect}>Finance App</h2>
 
    </div>
@@ -19,6 +22,7 @@ const ResterForm = () => {
 <img className={style.sircle} src={sirclesvg}/>
 {/* <div className='blockForm'> </div> */}
 <form className={style.form} >
+<img  className= {style.imgCesh} src={Ceshsvg}/>
 					<label className = {style.list}>
 			             <img src={masage}/>
 						<input
@@ -38,7 +42,7 @@ const ResterForm = () => {
 						<input placeholder='Ваше имя' type="text"	name="number"	autoComplete="off"	className={style.input}/>
 					</label>
 <div class= {style.buttonBlok}>
-					<button className={style.button} type="submit">
+					<button className={style.button1} type="submit">
 						регистрация
 					</button>
                     <button className={style.button} type="submit">
