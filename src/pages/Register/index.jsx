228ac3ce62c6b40c18fa01a606ac37f6d2701025
nomiso2000 <nomiso.study@gmail.com>
2020-12-02 22:19:06 +0200

    Register style

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -12,6 +12,8 @@ import masage from '../../styles/css/icon/masage.svg';
 import profil from '../../styles/css/icon/profil.svg';
 import regist from '../../styles/css/icon/register.svg';
 import sirclesvg from '../../styles/css/icon/orangesircle.svg';
+import Ceshsvg from '../../styles/css/icon/Cesh.svg';
+import fioletsvg from '../../styles/css/icon/fiolet.svg';
 const Register = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -77,12 +79,14 @@ const Register = () => {
     <div className={style.blockCover}>
       <div className={style.firstblock}>
         <img className={style.imgrigister} src={selectsvg} />
+        <img className={style.fiolet} src={fioletsvg} />
         <h2 className={style.titleselect}>Finance App</h2>
       </div>
       <div className={style.endblock}>
         <img className={style.sircle} src={sirclesvg} />
         {/* <h1 className={styles.header}>Register page</h1>  */}
         <form onSubmit={handleSubmit} className={style.form}>
+          <img className={style.imgCesh} src={Ceshsvg} />
           <label className={style.list}>
             <img src={masage} />
             <input
